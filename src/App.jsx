e19baf55f149a2fc61b0980d955ce63d5cb67346
Jimena Belen <jiment4@hotmail.com>
2023-06-11 07:59:23 -0300

    carrito

--- src/App.jsx
+++ src/App.jsx
@@ -25,40 +25,53 @@ import Cart from "./assets/icon-cart.svg";
 import Avatar from "./assets/image-avatar.png";
 
 function App() {
-  const [count, setCount] = useState(1);
+    const [count, setCount] = useState(1);
     const [foto, setFoto] = useState(Product1);
-
     const [show, setShow] = useState(false);
-
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-
     const [showA, setShowA] = useState(false);
+    const [carrito, setCarrito] = useState(0);
+    const [carritoVacio, setCarritoVacio] = useState(true);
+    const resultado = (count * 125).toFixed(2);
 
-    const toggleShowA = () => setShowA(!showA);
-
+    const handleFotos = (index) => {
+        setFoto(fotos[index].foto);
+        
+    };
 
     const fotos = [
         {
-          foto: Product1,
-          xs: Product1XS
+            foto: Product1,
+            xs: Product1XS
         },
         {
-          foto: Product2,
-          xs:Product2XS
+            foto: Product2,
+            xs:Product2XS
         },
         {
-          foto: Product3,
-          xs: Product3XS
+            foto: Product3,
+            xs: Product3XS
         },
         {
             foto: Product4,
             xs: Product4XS
+        }
+    ];
+
+    function handleDelete() {
+        setCarrito(0)
+        setCarritoVacio(true);
+      }
+
+    function toggleShowA () {
+        setShowA(!showA);
+        setCarrito(count);
+
+        if (carritoVacio) {
+            setCarritoVacio(false);
           }
-      ];
-      const handleFotos = (index) => {
-        setFoto(fotos[index].foto);
-      };
+    }
 
     function handleMinus() {
         if (count > 1) {
@@ -70,10 +83,9 @@ function App() {
         setCount(count + 1);
     }
     
-    const resultado = count * 125;
-  return (
+    return (
     <>
-      <Container>
+    <Container fluid>
         <Row>
             <Col>
                 <div className="nav-1005">
@@ -93,10 +105,10 @@ function App() {
                     <div className="icons-container">
                         <div className="cart-cont" onClick={handleShow}>                        
                             <div>
-                              <img src={Cart} className="cart-icon" />
+                                <img src={Cart} className="cart-icon" />
                             </div>
                             <div className='or-count'>
-                              <p>{count}</p>
+                                <p>{carrito}</p>
                             </div>
                             
                         </div>
@@ -107,10 +119,8 @@ function App() {
                 </div>
             </Col>
         </Row>
-    </Container>
-    <Container fluid>
-            <Row className='main-cont'>
-                <Col>
+        <Row className='main-cont'>
+            <Col>
                     <div className="image-container">
                         <div className="image-wrapper">
                             <img src={foto}/>
@@ -120,92 +130,96 @@ function App() {
                         
                         {fotos.map((pic, index) => (
                             <div key={index}>
-                                <div className="product-image" onClick={() => handleFotos(index)}>
+                                <div className="product-image" onMouseOver={() => handleFotos(index)}>
                                     <img src={pic.xs}/>
                                 </div>
                             </div>
                         ))}
                         </div>
                     </div>
-                </Col>
-                <Col>
-                    <div>
-                        <div className="text-container">
-                            <span>Sneaker Company</span>
-                            <h1>Fall Limited Edition Sneakers</h1>
-                        </div>
-                        <div className="product-details">
-                            <p className="product-description">These low-profile sneakers are your perfect casual wear companion. Featuring a 
+            </Col>
+            <Col>
+                <div>
+                    <div className="text-container">
+                        <span>Sneaker Company</span>
+                        <h1>Fall Limited Edition Sneakers</h1>
+                    </div>
+                    <div className="product-details">
+                        <p className="product-description">These low-profile sneakers are your perfect casual wear companion. Featuring a 
                                 durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
-                            <div className='product-price-container'>
-                                <p className="product-price">$125.00</p>
-                                <span className="product-discount">50%</span>
-                            </div>
-                             
-                            <p className="product-original-price">$250.00</p>
+                        <div className='product-price-container'>
+                            <p className="product-price">$125.00</p>
+                            <span className="product-discount">50%</span>
                         </div>
-                        <div className="product-actions">
-                            <div className="rating-container">
-                                <div className="rating-icon" onClick={handleMinus}>
-                                    <img src={Minus}   />
-                                </div>
+                        <p className="product-original-price">$250.00</p>
+                    </div>
+                    <div className="product-actions">
+                        <div className="rating-container">
+                            <div className="rating-icon" onClick={handleMinus}>
+                                <img src={Minus}   />
+                            </div>
                                 <div className="rating-value">
                                     <p>{count}</p>
-                                </div>
-                                <div className="rating-icon" onClick={handlePlus}>
-                                    <img src={Plus}  />
-                                </div>
                             </div>
-                            <div className="add-to-cart">
-                                <button className="icon-button" onClick={toggleShowA}>
-                                    <img src={Cart} />
-                                    Add to cart
-                                </button>
+                            <div className="rating-icon" onClick={handlePlus}>
+                                <img src={Plus}  />
                             </div>
                         </div>
+                        <div className="add-to-cart">
+                            <button className="icon-button" onClick={toggleShowA}>
+                                <img src={Cart} />
+                                <p>Add to cart</p>
+                            </button>
+                        </div>
                     </div>
-                </Col>
-            </Row>
-        </Container>
-        <Offcanvas show={show} onHide={handleClose} placement="end">
+                </div>
+            </Col>
+        </Row>
+    </Container>
+    <Offcanvas show={show} onHide={handleClose} placement="end">
         <Offcanvas.Header className='hr-cart' closeButton>
             <Offcanvas.Title>Cart</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-            <div className="offcn-container">
-                <div className="offcn-image-container">
+            {carritoVacio ? (
+                <div className='empt-flex'>
+                    <p className='empt-par'>Your cart is empty.</p>
+                </div>
+            
+                ) : (
                     <div>
-                        <img src={Product1XS} className="offcn-product-image" />
+                        <div className="offcn-container">
+                            <div className="offcn-image-container">
+                                <div>
+                                    <img src={Product1XS} className="offcn-product-image" />
+                                </div>
+                                <div className="offcn-product-details">
+                                    <p className="offcn-product-name">Fall Limited Edition Sneakers</p>
+                                    <p className="offcn-product-count">$125.00 x {count}</p>
+                                    <p className='result-count'> Total: ${resultado}</p>
+                                </div>
+                                <div className="offcn-icon-container" >
+                                    <img src={IconDelete} onClick={handleDelete} className="offcn-icon-image" />
+                                </div>
+                            </div>
+                            <div>
+                                <button className="offcn-checkout-button">Checkout</button>
+                            </div>
+                        </div>
                     </div>
-                    <div className="offcn-product-details">
-                        <p className="offcn-product-name">Fall Limited Edition Sneakers</p>
-                        <p className="offcn-product-count">$125.00 x {count}</p>
-                        <p className='result-count'> Total: ${resultado}</p>
-                    </div>
-                    <div className="offcn-icon-container">
-                        <img src={IconDelete} className="offcn-icon-image" />
-                    </div>
-                </div>
-                <div>
-                    <button className="offcn-checkout-button">Checkout</button>
-                </div>
-            </div>
+                )}
         </Offcanvas.Body>
     </Offcanvas>
     <ToastContainer position="top-end">
-      <Toast show={showA} onClose={toggleShowA} delay={3000} autohide>
-          <Toast.Header>
-            <strong className="me-auto">Congrats!</strong>
-          </Toast.Header>
-          <Toast.Body>Woohoo, se agregó tu producto al carrito!</Toast.Body>
-      </Toast>
+        <Toast show={showA} onClose={toggleShowA} delay={3000} autohide>
+            <Toast.Header>
+            <strong className="me-auto">Woohoo!</strong>
+            </Toast.Header>
+            <Toast.Body>Your product has been added to the cart!</Toast.Body>
+        </Toast>
     </ToastContainer>
-      
-
-    
-    
     </>
-  )
+);
 }
 
 export default App;
